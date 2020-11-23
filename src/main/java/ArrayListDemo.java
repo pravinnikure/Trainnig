e4133fa465c8cc052d358cf2dfaf808da575ec37
pravinnikure <pravinnikure300@gmail.com>
2020-11-23 16:34:57 +0530

    Simple ArrayList operation with Integers

---
+++ src/main/java/ArrayListDemo.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo
+{
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        for(int i=0;i<list.size();i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+
+}
